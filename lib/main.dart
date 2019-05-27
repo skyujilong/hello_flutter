c83464f6d4086e6f391eba0a99c33097cab356d8
skyujilong <skyujilong@163.com>
2019-05-27 18:22:40 +0800

    添加父类控制子类的方式

--- lib/main.dart
+++ lib/main.dart
@@ -1,157 +1,78 @@
+//父控件，管理子控件的状态
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+//入口的文件需要是 如下的这种title home 的 materialApp
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // title 布局是， 一个row， 一个column元素 一个icon元素 一个text元素
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground!',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text('Kandersteg, Switzerland',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                    ))
-              ],
-          )),
-          // Icon(
-          //   Icons.star,
-          //   color: Colors.red[500],
-          // ),
-          // Text('41')
-          FavoriteWidget(),
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-            softWrap: true,
-      ),
-    );
-    Color color = Theme.of(context).primaryColor;
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
     return MaterialApp(
-      title: '测试内容!',
+      title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout demo！'),
+          title: Text('Flutter Demo'),
         ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
+        body: Center(
+          child: ParentWidget(),
         ),
       ),
     );
   }
 }
 
-
-Column _buildButtonColumn(Color color,IconData icon, String label){
-  return Column(
-    mainAxisSize: MainAxisSize.min,  
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Icon(icon, color: color),
-      Container(
-        margin: const EdgeInsets.only(top: 8),
-        child:Text(
-          label,
-          style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            color: color,
-          )
-        )
-      )
-    ],
-  );
-}
-
-//给点击添加事件
-class FavoriteWidget extends StatefulWidget{
+class ParentWidget extends StatefulWidget{
   @override
-  _FavoriteWidgetState createState() {
-    // TODO: implement createState
-    return _FavoriteWidgetState();
+  _ParentWidget createState() {
+    return _ParentWidget();
+  }
+}
+class _ParentWidget extends State<ParentWidget>{
+  bool _active = false;
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,// 当前管理的state
+        onChanged: _handleTapboxChanged,//自己的方法
+      ),
+    );
   }
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget>{
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
 
-  //点击事件
-  void _toggleFavorite(){
-    setState(() {
-      if(_isFavorited){
-        _favoriteCount -= 1;
-      }else{
-        _favoriteCount += 1;
-      }
-      _isFavorited = !_isFavorited;
-    });
+class TapboxB extends StatelessWidget{
+  TapboxB({Key key,this.active:false,@required this.onChanged}):super(key:key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child:IconButton(
-            icon:_isFavorited?Icon(Icons.star):Icon(Icons.star_border),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          ),
+    //手势处理 GestureDetector
+    return GestureDetector(
+      onTap: _handleTap,
+      child:Container(
+        child: Text(
+          (active ? 'Active' : 'Inactive'),
+          style: TextStyle(fontSize: 32.0, color: Colors.white),
         ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child:Text('$_favoriteCount')
-          ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
         ),
-      ],
+      ),
     );
   }
 }
---
+++ lib/main2.dart
@@ -0,0 +1,157 @@
+//事件绑定，以及控件自行管理自己的state状态
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // title 布局是， 一个row， 一个column元素 一个icon元素 一个text元素
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground!',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text('Kandersteg, Switzerland',
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                    ))
+              ],
+          )),
+          // Icon(
+          //   Icons.star,
+          //   color: Colors.red[500],
+          // ),
+          // Text('41')
+          FavoriteWidget(),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+            softWrap: true,
+      ),
+    );
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      title: '测试内容!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo！'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+Column _buildButtonColumn(Color color,IconData icon, String label){
+  return Column(
+    mainAxisSize: MainAxisSize.min,  
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child:Text(
+          label,
+          style:TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color,
+          )
+        )
+      )
+    ],
+  );
+}
+
+//给点击添加事件
+class FavoriteWidget extends StatefulWidget{
+  @override
+  _FavoriteWidgetState createState() {
+    // TODO: implement createState
+    return _FavoriteWidgetState();
+  }
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  //点击事件
+  void _toggleFavorite(){
+    setState(() {
+      if(_isFavorited){
+        _favoriteCount -= 1;
+      }else{
+        _favoriteCount += 1;
+      }
+      _isFavorited = !_isFavorited;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child:IconButton(
+            icon:_isFavorited?Icon(Icons.star):Icon(Icons.star_border),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child:Text('$_favoriteCount')
+          ),
+        ),
+      ],
+    );
+  }
+}
