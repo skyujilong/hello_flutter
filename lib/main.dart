6727fd6697fe83adabcaef47c87ad6b7ceefce0d
skyujilong <skyujilong@163.com>
2019-05-27 17:35:41 +0800

    修改添加 按钮事件支持

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,12 @@ class MyApp extends StatelessWidget {
                     ))
               ],
           )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41')
+          // Icon(
+          //   Icons.star,
+          //   color: Colors.red[500],
+          // ),
+          // Text('41')
+          FavoriteWidget(),
         ],
       ),
     );
@@ -104,4 +105,53 @@ Column _buildButtonColumn(Color color,IconData icon, String label){
       )
     ],
   );
+}
+
+//给点击添加事件
+class FavoriteWidget extends StatefulWidget{
+  @override
+  _FavoriteWidgetState createState() {
+    // TODO: implement createState
+    return _FavoriteWidgetState();
+  }
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  //点击事件
+  void _toggleFavorite(){
+    setState(() {
+      if(_isFavorited){
+        _favoriteCount -= 1;
+      }else{
+        _favoriteCount += 1;
+      }
+      _isFavorited = !_isFavorited;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child:IconButton(
+            icon:_isFavorited?Icon(Icons.star):Icon(Icons.star_border),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child:Text('$_favoriteCount')
+          ),
+        ),
+      ],
+    );
+  }
 }
