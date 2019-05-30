203cc8f4fe5d807b9f6f3b9cf5dd7bd44a8e335e
skyujilong <skyujilong@163.com>
2019-05-30 18:02:44 +0800

    添加注视

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,17 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
+    // AnimationController 动画持续时间，以及防止超出屏幕依然进行动画渲染 (vsync)
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
+
     // #docregion addListener
+    // Tween 返回的是Animatable对象，.animate方法之后，返回的是animation 对象 Tween，控制变化值， animation.value 从0-300的变化
     animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      // 每次发生改变，都会主动触发addListener方法中注册的function函数。
       ..addListener(() {
         // #enddocregion addListener
-        setState(() {
+        setState(() { // 直接调用setState方法，进行刷新页面
           // The state that has changed here is the animation object’s value.
         });
         // #docregion addListener
